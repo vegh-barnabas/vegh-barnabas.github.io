e186b362e884630dbdd69a82ad728cb5bb9ae846
Végh Barnabás <62122932+vegh-barnabas@users.noreply.github.com>
2026-03-16 16:56:37 +0000

    add better confetti, add eslint and prettier run before deploy

--- src/routes/homepage/Homepage.tsx
+++ src/routes/homepage/Homepage.tsx
@@ -1,15 +1,40 @@
 import 'animate.css';
 
+import { useState } from 'react';
+import Confetti from 'react-confetti';
 import { Link } from 'react-router-dom';
 
 import styles from './Homepage.module.scss';
 
 function Homepage() {
+  const [runConfetti, setRunConfetti] = useState(false);
+  const [popped, setPopped] = useState(
+    Boolean(sessionStorage.getItem('popped')),
+  );
+
+  const popConfetti = () => {
+    if (!popped) {
+      setRunConfetti(true);
+      setPopped(true);
+      sessionStorage.setItem('popped', 'true');
+    }
+  };
+
   return (
     <>
+      <Confetti
+        run={runConfetti}
+        recycle={false}
+        numberOfPieces={140}
+        gravity={0.37}
+      />
       <div
         className={`content animate__animated animate__fadeIn ${styles.content}`}>
-        <h1>Welcome to my page!</h1>
+        <h1
+          onClick={popConfetti}
+          className={!popped ? `${styles.cursorEnabled}` : ''}>
+          Welcome to my page!
+        </h1>
         <p>
           Here you can access my personal resources as well as the lectures for
           the current year.
